88017bcbcbfb4219b133ea054fee45cb61eb6522
Guilherme Mendes Carvalho <gmendeca@emeal.nttdata.com>
2022-12-22 17:08:49 -0300

    att projeto

--- ProjetoXadrez/src/application/Program.java
+++ ProjetoXadrez/src/application/Program.java
@@ -1,14 +1,34 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Throwable {
 		
+		//retomar a partir da aula 190
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
-		//realizei at� a aula 188
+		
+		
+		while(true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performamChessMove(source, target);
+		}
+		
+
 	}
 
 }
--- ProjetoXadrez/src/chess/ChessMatch.java
+++ ProjetoXadrez/src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -22,6 +24,33 @@ public class ChessMatch {
 		}
 		return mat;
 	}
+	
+	public ChessPiece performamChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+		Position source = sourcePosition.toPosition();
+		Position target = sourcePosition.toPosition();
+		validateSourcePosition(source);
+		Piece capturedPiece = makeMove(source, target);
+		return(ChessPiece) capturedPiece;
+	}
+	
+	
+	private Piece makeMove(Position source, Position target) {
+		Piece p = board.removePeice(source);
+		Piece capturedPiece = board.removePeice(target);
+		board.placePiece(p, target);
+		return capturedPiece;
+	}
+	
+	
+	private void validateSourcePosition(Position position) {
+		if (!board.thereIsAPiece(position)) {
+			throw new ChessException("there is no piece on source position");
+		}
+	}
+	
+	
+	
+	
 	private void placeNewPeice(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
